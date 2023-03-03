db2ea69928522ea53605fc373cc3940b2c10df52
Tori <mes.viktoria@mail.ru>
2023-03-03 19:55:40 +0500

    lesson_11

---
+++ data.js
@@ -0,0 +1,40 @@
+const dataProducts = `
+    [
+        {   
+            "name": "MANGO  PEOPLE  T-SHIRT",
+            "img": "./img/product_card1.svg",
+            "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+            "price": "52.00"
+        },
+        {
+            "name": "MANGO  PEOPLE  T-SHIRT",
+            "img": "./img/product_card2.svg",
+            "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+            "price": "52.00"
+        },
+        {   
+            "name": "MANGO  PEOPLE  T-SHIRT",
+            "img": "./img/product_card3.svg",
+            "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+            "price": "52.00"
+        },
+        {   
+            "name": "MANGO  PEOPLE  T-SHIRT",
+            "img": "./img/product_card4.svg",
+            "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+            "price": "52.00"
+        },
+        {   
+            "name": "MANGO  PEOPLE  T-SHIRT",
+            "img": "./img/product_card5.svg",
+            "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+            "price": "52.00"
+        },
+        {   
+            "name": "MANGO  PEOPLE  T-SHIRT",
+            "img": "./img/product_card6.svg",
+            "description": "Known for her sculptural takes on traditional tailoring, Australian arbiter of cool Kym Ellery teams up with Moda Operandi.",
+            "price": "52.00"
+        }
+    ]
+`
